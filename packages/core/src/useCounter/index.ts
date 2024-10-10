3567cfbdcf05f8dc21db83abe93f0ce0f259d4b7
pyr33x <pyrexrj@gmail.com>
2024-10-10 20:10:38 +0330

    feat(hooks): useCounter

---
+++ packages/core/src/useCounter/index.ts
@@ -0,0 +1,32 @@
+import {
+  useCallback,
+  useState,
+  type SetStateAction,
+  type Dispatch,
+} from "react";
+
+type CounterReturnType = {
+  increment: () => void;
+  decrement: () => void;
+  reset: () => void;
+  count: number;
+  setCount: Dispatch<SetStateAction<number>>;
+};
+
+export function useCounter(initialValue?: number): CounterReturnType {
+  const [count, setCount] = useState(0);
+
+  const increment = useCallback(() => setCount((x) => x + 1), []);
+  const decrement = useCallback(() => setCount((x) => x - 1), []);
+  const reset = useCallback(() => {
+    setCount(initialValue ?? 0);
+  }, [initialValue]);
+
+  return {
+    increment,
+    decrement,
+    reset,
+    count,
+    setCount,
+  };
+}

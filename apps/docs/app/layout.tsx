fdf2503edfaaf210568c473e1a9c6d24dbc73d7e
pyr33x <pyrexrj@gmail.com>
2024-10-11 22:31:34 +0330

    chore(fonts): font variables

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,16 +1,27 @@
-import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
+import { JetBrains_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
+import "./global.css";
+
+const jetbrains = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 
 const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="selection:text-fd-foreground selection:bg-neutral-800">
+    <html
+      lang="en"
+      className={`${inter.variable} ${jetbrains.variable}`}
+      suppressHydrationWarning
+    >
+      <body className="dark:selection:text-fd-foreground selection:bg-neutral-800 selection:text-white dark:selection:bg-neutral-800">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>

40bec84159479ebd40cec0f6a1ab673a7a60c3e8
pyr33x <pyrexrj@gmail.com>
2024-10-11 22:30:43 +0330

    feat(root): implement the hero

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -1,16 +1,10 @@
+import { Hero } from "@/components/internal";
+import { Button } from "@/components/ui";
+
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col justify-center px-8 text-center">
-      <h1 className="text-balance text-4xl font-black lg:text-5xl">
-        Welcome to{" "}
-        <span className="animate-background-shine inline-flex transform bg-[linear-gradient(110deg,#0062D1,45%,#9d9ffc,55%,#0062D1)] bg-[length:250%_100%] bg-clip-text text-transparent">
-          Rehooks
-        </span>
-      </h1>
-      <p className="mt-1 text-balance text-base text-white/50 lg:text-xl">
-        An optimized, lightweight, and flexible react production-ready hooks
-        library written in TypeScript.
-      </p>
+    <main className="flex h-screen flex-col items-center justify-center px-8 text-center">
+      <Hero />
     </main>
   );
 }
---
+++ apps/docs/components/internal/hero.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useClipboard } from "rehooks-ts";
+import { Button } from "@/components/ui";
+import Link from "next/link";
+
+export function Hero() {
+  const { copy, isCopied } = useClipboard();
+  return (
+    <>
+      <h1 className="text-balance text-4xl font-black lg:text-5xl">
+        Welcome to{" "}
+        <span className="animate-background-shine inline-flex transform bg-[linear-gradient(110deg,#0062D1,45%,#9d9ffc,55%,#0062D1)] bg-[length:250%_100%] bg-clip-text text-transparent">
+          Rehooks
+        </span>
+      </h1>
+      <p className="dark:text-fd-muted-foreground/50 text-fd-muted-foreground mt-1 text-balance text-base lg:text-xl">
+        An optimized, lightweight, and flexible react production-ready hooks
+        library written in TypeScript.
+      </p>
+      <div className="mt-5 flex flex-row flex-wrap justify-center gap-2">
+        <Button
+          variant="hero"
+          size="circular"
+          onClick={() => copy("bun add rehooks-ts")}
+          className="font-mono"
+        >
+          {isCopied ? "Copied to Clipboard!" : "$ bun add rehooks-ts"}
+        </Button>
+        <Link href="/docs" className="outline-none ring-0">
+          <Button size="circular" variant="secondary" className="font-mono">
+            Checkout Docs
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
+}

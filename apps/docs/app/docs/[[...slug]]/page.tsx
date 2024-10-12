b70487d1882e0a3747370ef0a3b2e4f79c6e014d
pyr33x <pyrexrj@gmail.com>
2024-10-12 21:06:13 +0330

    feat(docs): Base

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -4,6 +4,7 @@ import {
   DocsTitle,
   DocsDescription,
 } from "fumadocs-ui/page";
+import { InlineTOC } from "fumadocs-ui/components/inline-toc";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import { getImageMeta } from "fumadocs-ui/og";
 import { notFound } from "next/navigation";
@@ -24,6 +25,7 @@ export default async function Page({
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
+      <InlineTOC items={page.data.toc} />
       <DocsBody>
         <MDX components={{ ...defaultMdxComponents }} />
       </DocsBody>

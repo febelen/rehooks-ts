382993ed9ed2ab14afaef7efad2efe6d47c7736e
pyr33x <pyrexrj@gmail.com>
2024-10-14 15:49:20 +0330

    feat(utils): constants

---
+++ packages/utils/src/constants/metadata.ts
@@ -0,0 +1,5 @@
+const NAME: string = "Rehooks";
+const DESCRIPTION: string =
+  "An optimized, lightweight, and reusable react production-ready hooks library written in TypeScript.";
+
+export { NAME, DESCRIPTION };

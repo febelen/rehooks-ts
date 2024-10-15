4ebdeb9bfc5c044a82c98a85db1ba504e8310fdc
pyr33x <pyrexrj@gmail.com>
2024-10-15 16:38:30 +0330

    chore(hooks): useKeyPress jsdoc

--- packages/core/src/useKeyPress/index.ts
+++ packages/core/src/useKeyPress/index.ts
@@ -7,6 +7,28 @@ interface KeyConfig {
   shift?: boolean;
 }
 
+/**
+ * Key configuration object for the useKeyPress hook.
+ * 
+ * @typedef {Object} KeyConfig
+ * @property {string} key - The key to listen for.
+ * @property {boolean} [ctrl] - Whether the Ctrl key must be pressed. Optional.
+ * @property {boolean} [alt] - Whether the Alt key must be pressed. Optional.
+ * @property {boolean} [shift] - Whether the Shift key must be pressed. Optional.
+ */
+
+/**
+ * Custom hook to detect if a specified key is pressed.
+ * 
+ * @param {KeyConfig} config - The configuration for the key press detection.
+ * @returns {boolean} - A boolean value indicating whether the specified key combination is currently pressed.
+ * 
+ * @example
+ * // Usage:
+ * const isEnterPressed = useKeyPress({ key: "Enter" });
+ * const isCtrlSPressed = useKeyPress({ key: "s", ctrl: true });
+ */
+
 export function useKeyPress(config: KeyConfig): boolean {
   const [keyPressed, setKeyPressed] = useState(false);
   const { key: targetKey, ctrl, alt, shift } = config;
@@ -48,4 +70,5 @@ export function useKeyPress(config: KeyConfig): boolean {
   }, []);
 
   return keyPressed;
-};
+}
+

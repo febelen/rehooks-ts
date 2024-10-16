6cd8883821e6ba6faddeb451ba21271bc9858c15
MrMeshky <mr.meshky@gmail.com>
2024-10-16 21:32:04 +0330

    feat: add jsDoc for useDebounceValue hook

--- packages/core/src/useDebounceValue/index.ts
+++ packages/core/src/useDebounceValue/index.ts
@@ -1,5 +1,18 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Custom hook that debounces a value, returning the value after the specified delay.
+ * Useful for cases where you want to prevent frequent updates (e.g., input fields).
+ *
+ * @template T - The type of the value being debounced.
+ * @param {T} value - The value to debounce.
+ * @param {number} delay - The debounce delay in milliseconds.
+ * @returns {T} - The debounced value.
+ *
+ * @example
+ * const debouncedSearchTerm = useDebounceValue(searchTerm, 500);
+ * // The debouncedSearchTerm will update 500ms after the last change in searchTerm.
+ */
 export function useDebounceValue<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 

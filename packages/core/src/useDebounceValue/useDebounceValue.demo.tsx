fafcd5915a76f0718df976469150b124d119a604
MrMeshky <mr.meshky@gmail.com>
2024-10-16 21:35:45 +0330

    feat: add demo for useDebounceValue hook

--- packages/core/src/useDebounceValue/useDebounceValue.demo.tsx
+++ packages/core/src/useDebounceValue/useDebounceValue.demo.tsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from "react";
+import { useDebounceValue } from "./index";
+
 export default function Component() {
-  return <div></div>;
+  const [searchInput, setSearchInput] = useState("");
+  const debouncedSearchInput = useDebounceValue(searchInput, 500);
+
+  useEffect(() => {
+    if (debouncedSearchInput)
+      console.log("Debounced Search Input:", debouncedSearchInput);
+  }, [debouncedSearchInput]);
+
+  return (
+    <div>
+      <h2>Debounced Search</h2>
+      <input
+        type="text"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        placeholder="Type something to search..."
+      />
+      <p>Search Input: {searchInput}</p>
+      <p>Debounced Input: {debouncedSearchInput}</p>
+    </div>
+  );
 }

13597daeed90d2bffe80c6ba64534dcd8845bcf6
pyr33x <pyrexrj@gmail.com>
2024-10-20 00:46:28 +0330

    feat(api): handle cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,16 @@
 import { swagger } from "@elysiajs/swagger";
+import { cors } from "@elysiajs/cors";
 import { Elysia, t } from "elysia";
 import { hook } from "./hook";
 
-const app = new Elysia().use(swagger()).use(hook).listen(3000);
+const app = new Elysia()
+  // enable cors
+  .use(cors())
+  // swagger
+  .use(swagger())
+  // /hooks
+  .use(hook)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,

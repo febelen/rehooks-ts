743f6fad5eb20b64abfe70aaa1a3efed346ef08e
pyr33x <pyrexrj@gmail.com>
2024-10-20 00:44:27 +0330

    feat(api): index route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,9 @@
-import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { Elysia, t } from "elysia";
+import { hook } from "./hook";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(swagger()).use(hook).listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
